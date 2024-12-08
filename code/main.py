8fa1e8b9ca0b889a51f6d97094183066729f9b12
utullyca <113405012+utullyca@users.noreply.github.com>
2024-12-08 16:40:13 -0500

    syntax

--- code/main.py
+++ code/main.py
@@ -1,4 +1,3 @@
-import tensorflow as tf
 from model import ResNetModel
 
 
@@ -6,7 +5,6 @@ from model import ResNetModel
 def main():
     resnet = ResNetModel()
 
-    # Create the ResNet model
     resnet_model = resnet.create_resnet_model((128,128,3))
     resnet_model.summary()
     resnet_model.train_model()
